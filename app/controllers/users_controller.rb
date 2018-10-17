b26f878644059f5923a8ba6a6023beb842922f81
Panagiotis Christinakis <chri-pas@hotmail.com>
2018-10-17 08:16:09 +0000

    authentication - authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  
   before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 1)
@@ -20,7 +23,6 @@ class UsersController < ApplicationController
   end
   
   def edit
-    
   end
   
   def update
@@ -44,4 +46,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account."
+      redirect_to root_path
+    end
+  end
+  
 end
