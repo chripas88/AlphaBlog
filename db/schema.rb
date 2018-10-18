629b50f03092b968af00f27395b2c4bcaff0ea31
Panagiotis Christinakis <chri-pas@hotmail.com>
2018-10-18 07:43:25 +0000

    article-category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_100450) do
+ActiveRecord::Schema.define(version: 2018_10_18_073101) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
